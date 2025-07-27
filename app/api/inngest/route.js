6d1bcb3169a37fc5d116e54acbd1db0273a4239b
SamikshaMorya <moryasamiksha52@gmail.com>
2025-07-27 21:51:47 +0530

    first commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,5 @@
-import { serve } from "inngest/next";
-import {
-  inngest,
-  syncUserCreation,
-  syncUserDeletion,
-  syncUserUpdation
-} from "../../../config/inngest.js"; 
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "../../../config/inngest.js";
+
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
